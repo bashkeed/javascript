7bb9df5df97c5db105e5b399524018329c199307
zubair <bashirkwarra@gmail.com>
2024-07-03 14:42:39 +0100

    added selection control switch

--- js/main.js
+++ js/main.js
@@ -7,4 +7,21 @@ function checkColor(code) {
     console.log("incorrect code");
   }
 }
-checkColor(2)
+checkColor(2);
+
+function decodeColor(code) {
+  switch (code) {
+    case 1:
+      console.log("Red");
+      break;
+    case 2:
+      console.log("Yellow");
+      break;
+    case "x":
+      console.log("Green");
+      break;
+    default:
+      console.log("Unknown code");
+  }
+}
+decodeColor(x)
