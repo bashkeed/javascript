35804d178a4ad4847d9b11b5201a825cdaf354b7
zubair <bashirkwarra@gmail.com>
2024-07-04 17:36:39 +0100

    added arrays and objects

--- js/oddEven.js
+++ js/oddEven.js
@@ -1,18 +1,31 @@
-var numberCheck = 0;
+// var numberCheck = 0;
 
-function oddEven(numberCheck) {
-    let i = 0;
-    let sum = 0;
-  while (i <= numberCheck) {
-    if (numberCheck % 2 !== 0) {
-      console.log("not an even number");
-    } else {
-      sum += i ;
-      i += 2
-    }
-    }
-return sum;
+// function oddEven(numberCheck) {
+//     let i = 0;
+//     let sum = 0;
+//   while (i <= numberCheck) {
+//     if (numberCheck % 2 !== 0) {
+//       console.log("not an even number");
+//     } else {
+//       sum += i ;
+//       i += 2
+//     }
+
+//     }
+// return sum;
+// }
+// console.log(oddEven(5))
+// console.log(sum)
+
+// let num= [["sam","john","jones"],83,["kelvin","hart"],85,["durant","kevin"],93,72]
+// console.log(`${num[0][2]} is not ${num[0][0]} but ${num[4][1]} ${num[0][1]} is ${num[3]} years old  `)
+// console.log(num.push('zubair'))
+// console.log(num.length)
+
+let students = {
+    name : "bashir",
+    age :20,
+    grade: 100
+
 }
-console.log(oddEven(5))
-console.log(sum)
-
+console.log(students.name)
